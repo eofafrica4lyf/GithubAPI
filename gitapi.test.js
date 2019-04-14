ae30a8eba462403abb153555b2819b3bf0fbf896
eofafrica4lyf <aboderinemmanuel@gmail.com>
2019-04-13 17:19:40 -0700

    initialized test to ensure a string was passed and a response was gotten

--- gitapi.test.js
+++ gitapi.test.js
@@ -6,4 +6,8 @@ test('test for empty parameter',()=>{
 
 test('test to ensure that the parameter passed is a string',()=>{
     expect(gitapi([])).toBe('Invalid parameter was passed');
+});
+
+test('test to ensure that a valid string/username was passed and a response was gotten',()=>{
+    expect(gitapi('eofafrica4lyf')).toContain('full_name');
 })
